2876f6a0e0ef1d33bd70737afc5285da84ed60c4
Taeil <xoxodlf@gmail.com>
2019-02-10 21:19:34 +0900

    solve stq_1

---
+++ AlgorithmPython/stq_1.py
@@ -0,0 +1,24 @@
+#deque를 스택처럼 이용하여 푼 풀이.
+
+def solution(arrangement):
+    from collections import deque
+    answer = 0
+    dq = deque()
+    before = None
+    for i in arrangement:
+        if i == '(':
+            dq.append('(')
+        else:
+            if before == '(':
+                before = dq.pop()
+                answer += len(dq)
+            if before == ')':
+                dq.pop()
+                answer += 1
+        before = i
+    return answer
+
+
+st = "()(((()())(())()))(())"
+print(solution(st))
+
